dca85a84f803b2d7c55041d088b99d90ad13ab44
angeliacssianturi <116820073+angeliacssianturi@users.noreply.github.com>
2023-05-21 00:10:04 +0700

    Game

---
+++ Assets/PlayerController2.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController2 : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    public float speed;
+    public Text skor2;
+        int score = 0;
+
+        void Start() {
+            playerRigidbody = GetComponent<Rigidbody>();
+
+        }
+
+        void Update()
+        {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+           /* if (Input.GetKey (KeyCode.LeftArrow)){
+                transform.Translate (0,0,speed);
+            }
+            if (Input.GetKey (KeyCode.RightArrow)){
+                transform.Translate (0,0,-speed);
+            }
+            if (Input.GetKey (KeyCode.UpArrow)){
+                transform.Translate (speed,0,0);
+            }
+            if (Input.GetKey (KeyCode.DownArrow)){
+                transform.Translate (-speed,0,0);
+            }
+        }*/
+    
+    public GameObject LvWonUI;
+        void OnTriggerEnter(Collider obj){
+            if (obj.gameObject.tag == "target"){
+                Destroy (obj.gameObject);
+                score += 5;
+                skor2.text ="Skor :"+ score.ToString();
+            }
+            if (score == 500){
+                LvWonUI.SetActive(true);
+            }
+        }
+}
